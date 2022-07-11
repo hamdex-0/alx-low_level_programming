95013aa2bcedf4b0955e56037843ad178653b472
Bairoking <gs8004rtj5jj5j@gmail.com>
2022-07-11 08:40:23 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev - check the code for Alx School students.
+ * @s: pointer int type
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+int len;
+for (len = 0; *s != '\0'; len++)
+s++;
+
+s--;
+for (len = len; len != 0; len--)
+{
+_putchar(*s);
+s--;
+}
+
+_putchar('\n');
+}
