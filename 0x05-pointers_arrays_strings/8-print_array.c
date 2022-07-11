0100c216586e4b618fa5acb98562b34b2bc6e3c0
Bairoking <gs8004rtj5jj5j@gmail.com>
2022-07-11 13:09:53 -0700

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_array - check the code for Alx School students.
+ * @a: pointer int type
+ * @n: is a int variable
+ */
+void print_array(int *a, int n)
+{
+int i, inte;
+
+for (i = 0; i < n; i++)
+{
+
+inte = *(a + i);
+
+printf("%d", inte);
+
+if (i != n - 1)
+printf(", ");
+}
+
+printf("\n");
+}
