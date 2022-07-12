04a82a2b8c4cf3d1598d8a31a1808e0a921ae5f3
Bairoking <gs8004rtj5jj5j@gmail.com>
2022-07-11 18:52:18 -0700

    Task 8

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,8 +8,8 @@ void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
-void puts_half(char *str);
 void print_array(int *a, int n);
+void puts_half(char *str);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 #endif
