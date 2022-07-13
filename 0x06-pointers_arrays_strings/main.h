e3b5e43c6510b101b215a35840f2c0522dffe48a
Bairoking <gs8004rtj5jj5j@gmail.com>
2022-07-13 10:46:36 -0700

    Task 12

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,5 +12,5 @@ int _putchar(char c);
 char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size);
+sh: 1: l: not found
 #endif
