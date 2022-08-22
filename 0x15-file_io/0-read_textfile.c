2b0d0b09121a8bd14bc70688ddaf53ef037a3e3d
Bairoking <gs8004rtj5jj5j@gmail.com>
2022-08-22 11:45:42 -0700

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,38 +1,39 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
-
+#include <stdlib.h>
+#include "main.h"
 /**
- * read_textfile - Reads a textfile and prints it to the POSIX standard output.
- * @filename: The filename.
- * @letters: The number of letters to read and print.
- * Return: The actual number of letters it could read and print if everything
- *	   runs well. 0 otherwise.
+ * read_textfile - function reads a text file and prints it to standard output
+ * @filename: file
+ * @letters: number of letters to read and print
+ *
+ * Return: number of letters read and printed, 0 if fail
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buffer;
-	int fd, tmp = 1;
-	size_t read_size = 0;
+	int fd, n_read, n_write;
+	char *s;
 
-	printf("la taille est %ld", letters);
-	if (!filename)
+	if (filename == NULL)
+		return (0);
+	s = malloc(letters * sizeof(char) + 1);
+	if (!s)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+	{
+		free(s);
 		return (0);
-	buffer = malloc(letters * sizeof(char));
-	buffer[letters] = '\0';
-	do {
-		tmp = read(fd, buffer, letters);
-		read_size += tmp;
-	} while (tmp);
+	}
+	n_read = read(fd, s, letters);
+	if (n_read == -1)
+	{
+		free(s);
+		return (0);
+	}
+	n_write = write(STDOUT_FILENO, s, n_read);
 	close(fd);
-	if (read_size != letters)
-		return (0);
-	write(1, buffer, letters);
-	return (read_size);
+	free(s);
+	if (n_read == n_write)
+		return (n_write);
+	return (0);
 }
