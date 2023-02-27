873f4b66a8192fe7a7ddbce2294519d115c629c2
hamdi abdulaziz <111043463+hamdex-0@users.noreply.github.com>
2023-02-27 09:54:51 +0300

    newer commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,26 +1,21 @@
-#include "holberton.h"
-#include <stdio.h>
-
+#include "main.h
 /**
-  * print_array - Prints n elements of an array of integers
-  * @a: Array of integers
-  * @n: Number of elements of the array to be printed
-  *
-  * Return: void
-  */
+* print_array - check the code for Alx School students.
+* @a: pointer int type
+* @n: is a int variable
+* Return: Always 0.
+*/
 void print_array(int *a, int n)
 {
-	int j;
+int i;
 
-	for (j = 0; j < n; j++)
-	{
-		printf("%d", a[j]);
+for (i = 0; i < n; i++)
 
-		if (j != (n - 1))
-		{
-			printf(", ");
-		}
-	}
+printf(' %d, ', a[i]);
 
-	printf("\n");
+if (i == (n - 1))
+printf('%d', a[n-1]);
+
+printf("\n");
 }
+
